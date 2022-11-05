75c8b11fe6881978ad93f2e1590fb0868bf8c45a
Abdellaharrad <arradabdellah1@gmail.com>
2022-11-05 15:39:13 +0100

    feat: create CustomerService

---
+++ TP3_Architectures_micro_service/customer-service/src/main/java/org/sid/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,30 @@
+package org.sid.customerservice;
+
+import org.sid.customerservice.entities.Customer;
+import org.sid.customerservice.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
+	@Bean
+	CommandLineRunner start(CustomerRepository customerRepository){
+		return args -> {
+			customerRepository.save(new Customer(null,"Abdellah","abdellah@gmail.com"));
+			customerRepository.save(new Customer(null,"Oussama","Oussama@gmail.com"));
+			customerRepository.save(new Customer(null,"Hamza","Hamza@gmail.com"));
+			customerRepository.findAll().forEach(c->{
+						System.out.println(c.toString());
+
+					}
+					);
+		};
+	};
+
+}
